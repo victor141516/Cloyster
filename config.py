5cfa8f5a0813fcbfbcd06b19bd4de0f6718b3ccb
Víctor <victor@shuttlecloud.com>
2018-08-28 17:31:14 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
+REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
+REDIS_DB = int(os.environ.get('REDIS_DB', 0))
+
+WEBS_DIR = 'webs'
---
+++ main.py
@@ -0,0 +1,123 @@
+from config import (
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_DB,
+    WEBS_DIR
+)
+from flask import Flask, request, redirect, make_response, abort
+import hashlib
+import logging
+import os
+from redpie import Redpie
+from shutil import rmtree
+from slugify import slugify
+import zipfile
+
+
+app = Flask('Cloyster', static_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), WEBS_DIR))
+db = Redpie(REDIS_DB, REDIS_HOST, REDIS_PORT)
+LOGGER = logging.getLogger()
+
+with open('static/upload.html', 'r') as f:
+    upload_web_html = f.read()
+
+with open('static/request_password.html', 'r') as f:
+    request_password_html = f.read()
+
+
+def unzip_to_webs_and_check(f, name):
+    dest_dir = WEBS_DIR + '/' + name
+    if os.path.isdir(dest_dir):
+        return False
+
+    try:
+        zf = zipfile.ZipFile(f)
+    except zipfile.BadZipFile:
+        return False
+
+    try:
+        zf.extractall(dest_dir)
+    except Exception as e:
+        LOGGER.error('Error extracting aparent good zip file', e)
+        return False
+
+    if os.path.exists(dest_dir + '/index.html'):
+        return True
+    else:
+        rmtree(dest_dir)
+        return False
+
+
+def encode_p(p):
+    return hashlib.sha256(p.encode('utf-8')).hexdigest()
+
+
+@app.route('/', methods=['GET'])
+def upload_web():
+    return upload_web_html
+
+
+@app.route('/', methods=['POST'])
+def upload_web_post():
+    if 'web_zip' not in request.files:
+        return redirect(request.url)
+    web_zip = request.files['web_zip']
+    if not web_zip.filename.endswith('.zip'):
+        return redirect(request.url)
+
+    if 'password' not in request.form:
+        return redirect(request.url)
+    password = request.form['password']
+
+    if 'web_name' not in request.form:
+        return redirect(request.url)
+    web_name = slugify(request.form['web_name'])
+
+    is_ok = unzip_to_webs_and_check(web_zip, web_name)
+    if not is_ok:
+        return redirect(request.url)
+
+    db[web_name] = encode_p(password)
+    return redirect('/' + web_name)
+
+@app.route('/setp', methods=['POST'])
+def ser_cookie():
+    response = redirect(request.form['path'])
+    if 'password' in request.form:
+        response.set_cookie('p', value=encode_p(request.form['password']))
+    return response
+
+
+@app.route('/<web_name>', defaults={'subpath': ''})
+@app.route('/<web_name>/', defaults={'subpath': ''})
+@app.route('/<web_name>/<path:subpath>')
+def give_web(web_name, subpath):
+    print(" -- 'p' in request.cookies =", 'p' in request.cookies)
+    print(" -- " + web_name + " in db =", web_name in db)
+    if web_name in db:
+        print(" -- " + request.cookies['p'] + " == " + db[web_name] + " =", request.cookies['p'] == db[web_name])
+
+    if 'p' in request.cookies and web_name in db and request.cookies['p'] == db[web_name]:
+        print(" -- Auth ok")
+        if os.path.isfile(WEBS_DIR + '/' + web_name + '/' + subpath):
+            print(" -- Is file")
+            return app.send_static_file(web_name + '/' + subpath)
+        elif os.path.isdir(WEBS_DIR + '/' + web_name + '/' + subpath):
+            print(" -- Is dir")
+            if os.path.isfile(WEBS_DIR + '/' + web_name + '/' + subpath + '/index.html'):
+                print(" -- index.html in dir")
+                return app.send_static_file(web_name + '/' + subpath + '/index.html')
+            else:
+                print(" -- index.html NOT in dir")
+                return abort(404)
+        else:
+            print(" -- Path doesnt exists")
+            return abort(404)
+    else:
+        print(" -- Auth miss")
+        response = make_response(request_password_html)
+        return response
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 12121, True)
